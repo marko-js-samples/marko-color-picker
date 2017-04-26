7af2a4b150898f6cea4e19d193419c8509460605
austinkelleher <austin.kell47@gmail.com>
2017-04-26 19:04:15 -0400

    4 spaces to 2 spaces.

--- components/color-picker/components/color-picker-footer/component.js
+++ components/color-picker/components/color-picker-footer/component.js
@@ -1,25 +1,25 @@
 function isValidHexValue(hexValue) {
-    return /^#[0-9A-F]{6}$/i.test(hexValue);
+  return /^#[0-9A-F]{6}$/i.test(hexValue);
 }
 
 module.exports = {
-    onInput(input) {
-        input.colors = input.colors || ['red', 'green', 'blue'];
-    },
-    onColorSelected(backgroundColor) {
-        this.emit('colorSelected', backgroundColor);
-    },
-    onHexInput() {
-        var hexInput = this.getEl('hexInput').value;
+  onInput(input) {
+    input.colors = input.colors || ['red', 'green', 'blue'];
+  },
+  onColorSelected(backgroundColor) {
+    this.emit('colorSelected', backgroundColor);
+  },
+  onHexInput() {
+    const hexInput = this.getEl('hexInput').value;
 
-        if (!hexInput.startsWith('#')) {
-            hexInput = '#' + hexInput;
-        }
-
-        if (!isValidHexValue(hexInput)) {
-            hexInput = this.input.colors[0];
-        }
-
-        this.emit('colorSelected', hexInput);
+    if (!hexInput.startsWith('#')) {
+      hexInput = '#' + hexInput;
     }
+
+    if (!isValidHexValue(hexInput)) {
+      hexInput = this.input.colors[0];
+    }
+
+    this.emit('colorSelected', hexInput);
+  }
 };
