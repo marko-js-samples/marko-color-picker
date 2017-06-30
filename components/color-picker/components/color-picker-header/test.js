d5cc7f59e866954553c608e51b62a9aa7bb6f0b9
Austin Kelleher <austin.kell47@gmail.com>
2017-06-30 09:23:42 -0400

    Fixes #7 - Failing test on color-picker-header

--- components/color-picker/components/color-picker-header/test.js
+++ components/color-picker/components/color-picker-header/test.js
@@ -9,7 +9,10 @@ test('color-picker-header color', function (context) {
   expect(output.$('div').attr('style')).to.contain('background-color:#000000');
 });
 
-test('color-picker-header default color', function (context) {
-  const output = context.render();
-  expect(output.$('div').attr('style')).to.contain('background-color:#333745');
+test('color-picker-header class included', function (context) {
+  const output = context.render({
+    color: '#000000'
+  });
+
+  expect(output.$('div').attr('class')).to.equal('color-picker-header');
 });
